d714dfe21e4c134e329da3a54cff0f87eea11939
Samiksha <182090251+t-r-samiksha@users.noreply.github.com>
2026-01-10 21:37:21 +0530

    Dashboard and reports done backend

--- client/src/services/admin/dashboard.service.js
+++ client/src/services/admin/dashboard.service.js
@@ -1,13 +1,26 @@
-import { dashboardMock } from '../../data/admin/dashboard.mock';
+// import { dashboardMock } from '../../data/admin/dashboard.mock';
+
+// export const fetchDashboardData = async () => {
+//     // Simulate API call
+//     // const response = await fetch('/api/admin/dashboard');
+//     // return response.json();
+
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             resolve(dashboardMock);
+//         }, 700);
+//     });
+// };
 
 export const fetchDashboardData = async () => {
-    // Simulate API call
-    // const response = await fetch('/api/admin/dashboard');
-    // return response.json();
+    const response = await fetch('http://127.0.0.1:8000/api/admin/dashboard');
 
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve(dashboardMock);
-        }, 700);
-    });
+    if (!response.ok) {
+        throw new Error('Failed to fetch dashbaord data');
+    }
+
+    const result = await response.json();
+
+    // API returns { success, count, data }
+    return result;
 };
--- client/src/services/admin/reports.service.js
+++ client/src/services/admin/reports.service.js
@@ -1,13 +1,26 @@
-import { reportsMock } from '../../data/admin/reports.mock';
+// import { reportsMock } from '../../data/admin/reports.mock';
+
+// export const fetchReportsData = async () => {
+//     // Simulate API call
+//     // const response = await fetch('/api/admin/reports');
+//     // return response.json();
+
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             resolve(reportsMock);
+//         }, 500);
+//     });
+// };
 
 export const fetchReportsData = async () => {
-    // Simulate API call
-    // const response = await fetch('/api/admin/reports');
-    // return response.json();
+    const response = await fetch('http://127.0.0.1:8000/api/admin/reports');
 
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve(reportsMock);
-        }, 500);
-    });
+    if (!response.ok) {
+        throw new Error('Failed to fetch reports data');
+    }
+
+    const result = await response.json();
+
+    // API returns { success, count, data }
+    return result;
 };
